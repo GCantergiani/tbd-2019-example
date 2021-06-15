2d06125d7a33588edea5a4d0c37bab22545d1c45
pablofbrtl <pigonzalez@falabella.cl>
2021-06-15 19:37:59 -0400

    basic post example

--- api_rest/main.py
+++ api_rest/main.py
@@ -25,10 +25,22 @@ class Students(Resource):
     def get(self):
         db_connect = create_engine('sqlite:///{}'.format(NAME_DATABASE))
         conn = db_connect.connect()
-        query = conn.execute('SELECT FIRST_NAME, LAST_NAME, AGE FROM STUDENT')
+        query = conn.execute('SELECT FIRST_NAME, LAST_NAME, AGE, TYPE_ID FROM STUDENT')
 
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        id_user = json_data['id_user']
+        id_type = json_data['id_type']
+
+        db_connect = create_engine('sqlite:///{}'.format(NAME_DATABASE))
+        conn = db_connect.connect()
+        query = conn.execute(f"UPDATE STUDENT SET TYPE_ID = {id_type} WHERE ID = {id_user}")
+
+
+
 
 api.add_resource(Digits, '/digits')
 api.add_resource(Students, '/students')
@@ -40,18 +52,30 @@ def create_db_w_examples():
     cursor = conn.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS STUDENT")
+    cursor.execute("DROP TABLE IF EXISTS TYPE")
     conn.commit()
 
-    #Creating table as per requirement
-    ddl_query ='''
+    #Creating table as per requirement+
+    ddl_type = '''
+        CREATE TABLE TYPE(
+                ID INT NOT NULL,
+                VALUE CHAR(20) NOT NULL
+            )
+    '''
+
+    ddl_query_student ='''
         CREATE TABLE STUDENT(
+            ID INT,
             FIRST_NAME CHAR(20) NOT NULL,
             LAST_NAME CHAR(20),
-            AGE INT
+            AGE INT,
+            TYPE_ID INT,
+            FOREIGN KEY(TYPE_ID) REFERENCES TYPE(ID)
         )
-        '''
-    
-    cursor.execute(ddl_query)
+    '''
+
+    cursor.execute(ddl_type)
+    cursor.execute(ddl_query_student)
 
     logger.info('Students table created ...')
     conn.commit()
@@ -62,14 +86,23 @@ def create_db_w_examples():
         
         insert_query = '''
             INSERT INTO STUDENT
-            VALUES('{}', '{}', {})
-        '''.format(names.get_first_name(), 
+            VALUES({}, '{}', '{}', {}, NULL)
+        '''.format(x,
+                   names.get_first_name(), 
                    names.get_last_name(),
                    randrange(100))
         
         cursor.execute(insert_query)
 
         conn.commit()
+
+    for x in range(10):
+        logger.debug('Inserting {} types...'.format(x))
+        insert_query = '''
+            INSERT INTO TYPE
+            VALUES({type}, type_'{type}')
+        '''.format(type=x)
+
 
     conn.close()
 
